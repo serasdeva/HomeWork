13d8ac482a51574b761af6db2ccb3f732bc678ec
serasdeva <serasdeva@gmail.com>
2022-11-23 23:03:23 +0700

    TaskTwo - Второе задание

---
+++ src/lesson1/TaskTwo.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class TaskTwo {
+    public int[] buildArray(int[] nums) {
+        int[] newArray = new int[nums.length];
+        for (int i = 0; i < nums.length; i++){
+            newArray[i] = nums[nums[i]];
+        }
+        return newArray;
+    }
+}
