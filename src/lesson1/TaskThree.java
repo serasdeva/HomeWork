5ea9cbb783055b9a6df937649bcbc11ab13b4475
serasdeva <serasdeva@gmail.com>
2022-11-24 22:35:06 +0700

    TaskThree - Третье задание

---
+++ src/lesson1/TaskThree.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class TaskThree {
+    public int removeElement(int[] nums, int val) {
+        int point = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[point] = nums[i];
+                point++;
+            }
+        }
+        return point;
+    }
+}
--- src/org/geekbrains/Main.java
+++ src/org/geekbrains/Main.java
@@ -1,4 +1,25 @@
 package org.geekbrains;
 
+import lesson1.TaskThree;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
+    public static int[] removeElement(int[] arr, int item) {
+        return Arrays.stream(arr)
+                .filter(i -> i != item)
+                .toArray();
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 2, 2, 3};
+        Scanner num = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int val = num.nextInt();
+
+        nums = removeElement(nums, val);
+
+        System.out.println(Arrays.toString(nums));
+
+    }
 }
