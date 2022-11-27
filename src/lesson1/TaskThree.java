5a32c90ee97b03cfea3c37153d3a889b07c546b2
serasdeva <serasdeva@gmail.com>
2022-11-27 12:04:44 +0700

    TaskThree - Третье задание

--- src/lesson1/TaskThree.java
+++ src/lesson1/TaskThree.java
@@ -3,7 +3,7 @@ package lesson1;
 import java.util.Scanner;
 
 public class TaskThree {
-    public static int removeElement(int[] nums, int val) {
+    public int removeElement(int[] nums, int val) {
         int point = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
